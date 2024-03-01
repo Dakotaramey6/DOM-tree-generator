4f9e836ef40c59d703536c843c2b8696cfa6359e
Dakota Ramey <Dakotaramey6@gmail.com>
2024-02-29 23:42:36 -0500

    Start up for code created

--- main-react/src/App.js
+++ main-react/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import Draggable from "react-draggable";
+
+import "./App.css";
 
 function App() {
+  const [content, setContent] = useState(null);
+  const [cards, setCards] = useState([{ content: "test", id: 0 }]);
+
+  const addCards = () => {
+    setCards(() => {});
+  };
+
+  const handleClick = ({ target }) => {
+    setContent(target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <List cards={cards} content={content} />
+    </>
   );
 }
+
+const List = ({ cards }) => {
+  return (
+    <div className="List">
+      {cards.map((card) => (
+        <Card content={card.content} key={card.id} />
+      ))}
+    </div>
+  );
+};
+
+const Card = ({ content }) => {
+  const nodeRef = useRef(null);
+  return (
+    <Draggable nodeRef={nodeRef}>
+      <div ref={nodeRef}>
+        <p className="exit">❌</p>
+        <p className="content">{content}</p>
+      </div>
+    </Draggable>
+  );
+};
 
 export default App;
